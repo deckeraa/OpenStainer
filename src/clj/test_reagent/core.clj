9b855f27c3f4eda0f951d3bb7022f31305a06e60
Aaron Decker <decker49@gmail.com>
2019-11-30 00:58:58 -0600

    Add pin controls and pulse controls. Get stepper spinning smoothly. Still figuring out how the DIR pin works.

--- src/clj/test_reagent/core.clj
+++ src/clj/test_reagent/core.clj
@@ -12,21 +12,73 @@
 (defn led-handler [req]
   (println req))
 
+(defonce state-atom (atom {}))
+
+(defn init-pins
+  ([] (init-pins state-atom))
+  ([state-atom]
+   (swap! state-atom assoc :device (gpio/device 0))
+   (swap! state-atom assoc :handle (gpio/handle (:device @state-atom)
+                                                {17 {::gpio/tag :ena}
+                                                 18 {::gpio/tag :dir}
+                                                 19 {::gpio/tag :pul}}
+                                                {::gpio/direction :output}))
+   (swap! state-atom assoc :buffer (gpio/buffer (:handle @state-atom)))))
+
+(defn clean-up-pins
+  ([] (clean-up-pins state-atom))
+  ([state-atom]
+   (gpio/close (:handle @state-atom))
+   (gpio/close (:device @state-atom))
+   (swap! state-atom dissoc :handle)
+   (swap! state-atom dissoc :device)
+   (swap! state-atom dissoc :buffer)))
+
+(defn set-pin [pin-tag state]
+  (when (not (:device @state-atom)) (init-pins))
+  (gpio/write (:handle @state-atom) (-> (:buffer @state-atom) (gpio/set-line pin-tag state))))
+
+(defn pin-handler [req]
+  (let [body (keywordize-keys (json/read-str (request/body-string req)))]
+    (println body)
+    (set-pin (keyword (:pin-tag body)) (:state body))
+    (content-type (response/response "<h1>Success.</h1>") "text/html"))
+  )
+
+(defn pulse [pin-tag wait-ms num-pulses]
+  (loop [i num-pulses]
+    (when (> i 0)
+      (set-pin pin-tag true)
+      (java.util.concurrent.locks.LockSupport/parkNanos (* 1000 wait-ms))
+      (set-pin pin-tag false)
+      (java.util.concurrent.locks.LockSupport/parkNanos (* 1000 wait-ms))
+;      (Thread/sleep wait-ms)
+      (recur (- i 1)))))
+
+(defn pulse-handler [req]
+  (let [body (keywordize-keys (json/read-str (request/body-string req)))]
+    (println body)
+    (pulse (keyword (:pin-tag body)) (Integer/parseInt (:wait-ms body)) (Integer/parseInt (:num-pulses body)))
+    (content-type (response/response "<h1>Success.</h1>") "text/html")))
+
 (defn blink [pin_num]
-  (let [device (gpio/device 0)
-        handle (gpio/handle device
-                            {pin_num {::gpio/tag :red}}
-                            {::gpio/direction :output})
-        buff (gpio/buffer handle)]
-    (println "Setting " pin_num " high")
-    (gpio/write handle (-> buff (gpio/set-line :red true)))
- ;   (println (gpio/describe-line device 17))
-    (Thread/sleep 2000)
-    (println "Setting " pin_num " low")
-    (gpio/write handle (-> buff (gpio/set-line :red false)))
-;    (println (gpio/describe-line device 17))
-    (gpio/close handle)
-    (gpio/close device)))
+  (try
+    (let [device (gpio/device 0)
+          handle (gpio/handle device
+                              {pin_num {::gpio/tag :red}}
+                              {::gpio/direction :output})
+          buff (gpio/buffer handle)]
+      (println "Setting " pin_num " high")
+      (gpio/write handle (-> buff (gpio/set-line :red true)))
+                                        ;   (println (gpio/describe-line device 17))
+      (Thread/sleep 2000)
+      (println "Setting " pin_num " low")
+      (gpio/write handle (-> buff (gpio/set-line :red false)))
+                                        ;    (println (gpio/describe-line device 17))
+      (gpio/close handle)
+      (gpio/close device))
+    (catch Exception e
+      (println e))))
 
 (defn blink-handler [req]
   (let [body (keywordize-keys (json/read-str (request/body-string req)))]
@@ -71,6 +123,8 @@
 (def api-routes
   ["/" [["led" led-handler]
         ["blink" blink-handler]
+        ["pin" pin-handler]
+        ["pulse" pulse-handler]
         [true (fn [req] (content-type (response/response "<h1>Hi from Pi.</h1>") "text/html"))]]])
 
 (def app
--- src/cljs/test_reagent/core.cljs
+++ src/cljs/test_reagent/core.cljs
@@ -28,14 +28,67 @@
                            (println "POST Resp: " resp))))
          } name])
 
+
+(defn pin-control [pin-tag]
+  (let [style {:background-color :red
+                   :color :white
+                   :font-size "40px"
+                   :margin "5px"
+                   :width "50px"
+               :height "50px"}]
+    [:tr
+     [:td (str pin-tag)]
+     [:td
+      [:div {:style style
+             :on-click (fn [e]
+                         (go (let [resp (http/post "http://localhost:3000/pin"
+                                                   {:json-params {:pin-tag pin-tag :state true}}
+                                                   :with-credentials? false)]
+                               (println "POST Resp: " resp))))
+             } "On"]]
+     [:td
+      [:div {:style style
+             :on-click (fn [e]
+                         (go (let [resp (http/post "http://localhost:3000/pin"
+                                                   {:json-params {:pin-tag pin-tag :state false}}
+                                                   :with-credentials? false)]
+                               (println "POST Resp: " resp))))
+             } "Off"]]]))
+
+(defn pulse-control [pin-tag]
+  (let [wait-ms    (reagent/atom 1)
+        num-pulses (reagent/atom 480)]
+    (fn []
+      [:tr (str pin-tag)
+       [:td [:input {:type "button" :value "Pulse"
+                     :on-click (fn [e]
+                                 (go (let [resp (http/post "http://localhost:3000/pulse"
+                                                           {:json-params {:pin-tag pin-tag :wait-ms @wait-ms :num-pulses @num-pulses}}
+                                                           :with-credentials? false)]
+                                       (println "POST resp: " resp))))}]]
+       [:td [:input {:type "text" :value @wait-ms
+                     :on-change #(reset! wait-ms (-> % .-target .-value))}]]
+       [:td [:input {:type "text" :value @num-pulses
+                     :on-change #(reset! num-pulses (-> % .-target .-value))}]]])))
+
+(defn pins-control []
+  (into
+   [:table [pulse-control :pul]]
+   (map pin-control [:ena :dir]))
+  )
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Page
 
 (defn page [ratom]
   [:div
    "Welcome to reagent-figwheel!!!!!!!!!!!"
-   [led-button "LED" 17]
-   [led-button 18 18]])
+   [pins-control]
+;   [led-button "LED" 17]
+;   [led-button 18 18]
+   ])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize App
