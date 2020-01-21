45611bbd3b6d8688e09b54334133932f78529539
Aaron Decker <decker49@gmail.com>
2020-01-21 10:06:32 -0600

    Add debouncing code.

--- src/clj/slide_stainer/core.clj
+++ src/clj/slide_stainer/core.clj
@@ -13,7 +13,7 @@
             [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia :as lacinia]
             [clojure.edn :as edn]
-            [clojure.core.async :as async :refer [go go-loop <! timeout thread chan mult tap]]
+            [clojure.core.async :as async :refer [go go-loop <! timeout thread chan mult tap put!]]
             [incanter.core :refer [pow]]
             [clojure.walk :as walk])
   (:use clojure.test)
@@ -37,7 +37,7 @@
                    :inverted? false}
                19 {::gpio/tag :stepperZ-pul
                    :inverted? false}}
-              :limit-switch-low  {:pin 4 :is-high-closed? true}
+              :limit-switch-low  {:pin 4 :invert? false}
               :travel_distance_per_turn 0.063
               :position nil
               :position_limit 9
@@ -254,17 +254,36 @@
   (let [gpio-chan (chan)
         gpio-multi-chan (mult gpio-chan) ; to support multiple subscribers to the events channel
         gpio-watcher (gpio/watcher
-                 (:device @state-atom)
-                 (get-input-pin-defs-for-gpio-lib pin-defs))
+                      (:device @state-atom)
+                      (get-input-pin-defs-for-gpio-lib pin-defs))
         last-event-timestamp (atom 0)
         status-atm (atom (init-status-atm gpio-watcher pin-defs))
         debounce-wait-time-ns (* 2 1000 1000)]
     ;; Add necessary atoms into the global state
     (swap! state-atom assoc :watcher gpio-watcher)
     (swap! state-atom assoc :status-atm status-atm)
+    (println "Added status-atm to global state: " @status-atm)
     (thread (while (:watcher @state-atom) ; this gets closed in clean-up-pins
-              (if-some [evt (gpio/event gpio-watcher 5000)]
-                (println "Event: " evt))))))
+              (if-some [evt (gpio/event gpio-watcher 1000)]
+                (do
+                  (swap! last-event-timestamp
+                         (fn [timestamp]
+                           (let [tag (:dvlopt.linux.gpio/tag evt)
+                                 evt-timestamp (:dvlopt.linux.gpio/nano-timestamp evt)
+                                 transitioning-to-state (= :rising (:dvlopt.linux.gpio/edge evt))
+                                 ]
+                             (if (and
+                                  ; since the GPIO library sometimes generates multiple events of same direction (i.e. two :falling events), we need to check and only count an actual state transition as the signal before starting the debounce timer
+                                  (not (= transitioning-to-state (get @status-atm tag)))
+                                  (> evt-timestamp
+                                     (+ timestamp debounce-wait-time-ns)))
+                               (do
+                                 (swap! status-atm (fn [s] (assoc s tag transitioning-to-state)))
+                                 (println "Swapped " tag @status-atm)
+                                 (put! gpio-chan evt)
+                                 evt-timestamp)
+                               timestamp))
+                           ))))))))
 
 (defn resolve-state [context args value]
   {:contents (str @state-atom)})
@@ -277,6 +296,7 @@
                                                 (get-output-pin-defs-for-gpio-lib (:setup @state-atom))
                                                 {::gpio/direction :output}))
    (swap! state-atom assoc :buffer (gpio/buffer (:handle @state-atom)))
+   (println "Calling init-watcher")
    (init-watcher pin-defs)
    ))
 
