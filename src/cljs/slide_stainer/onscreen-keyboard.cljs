e0c70d110cc8a8f3768ad6c07ebc2a2522526e8a
Aaron Decker <decker49@gmail.com>
2020-01-30 09:36:55 -0600

    Implement proper backspace support.

--- src/cljs/slide_stainer/onscreen-keyboard.cljs
+++ src/cljs/slide_stainer/onscreen-keyboard.cljs
@@ -24,32 +24,39 @@
 (defcard-rg osk-input-card
   [osk-input osk-atm {}])
 
+(defn osk-button
+  ([osk-atm val]
+   (osk-button osk-atm val nil nil))
+  ([osk-atm val display-name]
+   [osk-button osk-atm val display-name nil])
+  ([osk-atm val display-name special-click-handler]
+   (let [input-atm (:input-atm @osk-atm)
+         el-atm (:el-atm @osk-atm)]
+     ^{:key val}
+     [:button {:on-click (fn [e]
+                           (when el-atm (println @el-atm (.-selectionStart @el-atm)))
+                           (when (and input-atm el-atm)
+                             (let [cursor-pos (.-selectionStart @el-atm)]
+                               (if special-click-handler ; if we have a special handler (for example, Backspace)...
+                                 (special-click-handler e input-atm el-atm cursor-pos) ; ... then run that ...
+                                 (swap! input-atm (fn [v] ; ... otherwise, insert val at the cursor position
+                                                    (str (subs v 0 cursor-pos)
+                                                         val
+                                                         (subs v cursor-pos (count v)))))))))
+               :on-mouse-down (fn [e] ; try to avoid the event from bubbling so far as to take focus away from the input field
+                                (.preventDefault e))}
+      (if display-name display-name val)])))
+
 (defn onscreen-keyboard [osk-atm]
-  (let [button-fn (fn [val] 
-                    (let [input-atm (:input-atm @osk-atm)
-                          el-atm (:el-atm @osk-atm)]
-                      ^{:key val}
-                      [:button {:on-click (fn [e]
-                                            (when el-atm (println @el-atm (.-selectionStart @el-atm)))
-                                            (println "on-click" e @osk-atm)
-                                            (when (and input-atm el-atm)
-                                              (let [cursor-pos (.-selectionStart @el-atm)]
-                                                (swap! input-atm (fn [v]
-                                                                   (str (subs v 0 cursor-pos)
-                                                                        val
-                                                                        (subs v cursor-pos (count v))))))))
-                                :on-mouse-down (fn [e]
-                                                 (println "on-mousedown")
-                                                 (.preventDefault e))} val])) ]
+  (let [button-fn (partial osk-button osk-atm)]
     (fn [osk-atm]
       [:div
        [:div
         (doall (map button-fn (range 10)))
-        [:button {:on-click (fn [e]
-                              (println "backspace" @osk-atm)
-                              (when (:input-atm @osk-atm)
-                                (swap! (:input-atm @osk-atm) (fn [v] (subs v 0 (dec (count v))))))
-                              )} "Backspace"]]
+        (button-fn nil "Backspace" (fn [e input-atm el-atm cursor-pos]
+                                     (swap! input-atm (fn [v]
+                                                        (str (subs v 0 (dec cursor-pos))
+                                                             (subs v cursor-pos (count v)))))))]
        [:div
         (doall (map button-fn "qwertyuiop"))]
        [:div
@@ -57,8 +64,7 @@
        [:div
         (doall (map button-fn "zxcvbnm"))]
        [:div
-        (button-fn "Space")]
-       ])))
+        (button-fn " " "Space")]])))
 
 (defcard-rg onscreen-keyboard-card
   [onscreen-keyboard osk-atm])
