36bff958ff2e43a0f3946571a071e4b260e31400
Aaron Decker <decker49@gmail.com>
2020-01-30 09:17:56 -0600

    Add support for tracking osk input elements and inserting characters at the current cursor location. Space and Backspace still not supported for cursor input.

--- src/cljs/slide_stainer/onscreen-keyboard.cljs
+++ src/cljs/slide_stainer/onscreen-keyboard.cljs
@@ -5,15 +5,18 @@
    [devcards.core :refer [defcard defcard-rg]]))
 
 (defn osk-input [osk-atm args]
-  (let [input-atm (reagent/atom "foo")]
+  (let [input-atm (reagent/atom "foo")
+        ref-atm   (clojure.core/atom nil)]
     (fn []
       [:input {:type :text
                :on-focus (fn [e]
                            (swap! osk-atm assoc :input-atm input-atm) ; set the on-screen keyboard to point to this input field's input
+                           (swap! osk-atm assoc :el-atm ref-atm)
                            (println "on-focus" @osk-atm)
                            )
                :on-blur (fn [e] (println "BBBBBBBBBBBBBBBBBBBLLLLLLLLLLLLLLLLLLLLLLLUUUUUUUUUUUUUUUUUUUURRRRRRRRRRRRR"))
                :on-change (fn [e] (reset! input-atm (-> e .-target .-value)))
+               :ref (fn [el] (reset! ref-atm el))
                :value @input-atm}])))
 
 (def osk-atm (reagent/atom {}))
@@ -22,32 +25,37 @@
   [osk-input osk-atm {}])
 
 (defn onscreen-keyboard [osk-atm]
-  (let [button-fn (fn [val] [:button {:on-click (fn [e]
-                                                  (println "on-click" e @osk-atm)
-                                                  (when (:input-atm @osk-atm)
-                                                    (swap! (:input-atm @osk-atm) (fn [v] (str v val))))
-                                                  (println (reagent/current-component))
-                                                  (this-as this
-                                                    (println this))
-                                                  )
-                                      :on-mouse-down (fn [e]
-                                                       (println "on-mousedown")
-                                                       (.preventDefault e))} val]) ]
+  (let [button-fn (fn [val] 
+                    (let [input-atm (:input-atm @osk-atm)
+                          el-atm (:el-atm @osk-atm)]
+                      ^{:key val}
+                      [:button {:on-click (fn [e]
+                                            (when el-atm (println @el-atm (.-selectionStart @el-atm)))
+                                            (println "on-click" e @osk-atm)
+                                            (when (and input-atm el-atm)
+                                              (let [cursor-pos (.-selectionStart @el-atm)]
+                                                (swap! input-atm (fn [v]
+                                                                   (str (subs v 0 cursor-pos)
+                                                                        val
+                                                                        (subs v cursor-pos (count v))))))))
+                                :on-mouse-down (fn [e]
+                                                 (println "on-mousedown")
+                                                 (.preventDefault e))} val])) ]
     (fn [osk-atm]
       [:div
        [:div
-        (map button-fn (range 10))
+        (doall (map button-fn (range 10)))
         [:button {:on-click (fn [e]
                               (println "backspace" @osk-atm)
                               (when (:input-atm @osk-atm)
                                 (swap! (:input-atm @osk-atm) (fn [v] (subs v 0 (dec (count v))))))
                               )} "Backspace"]]
        [:div
-        (map button-fn "qwertyuiop")]
+        (doall (map button-fn "qwertyuiop"))]
        [:div
-        (map button-fn "asdfghjkl")]
+        (doall (map button-fn "asdfghjkl"))]
        [:div
-        (map button-fn "zxcvbnm")]
+        (doall (map button-fn "zxcvbnm"))]
        [:div
         (button-fn "Space")]
        ])))
