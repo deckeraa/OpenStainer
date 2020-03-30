b916f2f5444276cd0058d93f8c634dc423945009
Aaron Decker <aaron@stronganchortech.com>
2020-03-30 15:22:28 -0500

    Clean up code in structs_and_consts.rs

--- src/structs_and_consts.rs
+++ src/structs_and_consts.rs
@@ -9,21 +9,20 @@ use rocket::http::RawStr;
 pub type Inch = f64;
 pub type PulseCount = u64;
 
-pub const LEFT_POSITION: Inch = 0.35;
-pub const UP_POSITION: Inch = 3.5;
-pub const JAR_SPACING: Inch = 1.9;
+pub const LEFT_POSITION: Inch = 0.35; // This is the position the rack needs to be in on the x-axis to make it into the first staining jar.
+pub const UP_POSITION: Inch = 3.5; // This is the "up" position on the z-axis. This is important for getting the rack into the proper down position, since the limit switch is on the upper end of the z-axis.
+pub const JAR_SPACING: Inch = 1.9; // This is the distance between jars. Used to calculate the jar positioning.
 
 pub const COUCHDB_URL: &'static str = "http://localhost:5984/slide_stainer";
 //pub const COUCHDB_URL: &'static str = "http://localhost:5984/slide_stainer_demo";
-
 
 pub struct Stepper {
     pub ena: gpio::sysfs::SysFsGpioOutput,
     pub dir: gpio::sysfs::SysFsGpioOutput,
     pub pul: gpio::sysfs::SysFsGpioOutput,
-    pub limit_switch_low: Option<gpio::sysfs::SysFsGpioInput>,
+    pub limit_switch_low: Option<gpio::sysfs::SysFsGpioInput>, // these are Options since currently each axis only has one limit switch
     pub limit_switch_high: Option<gpio::sysfs::SysFsGpioInput>,
-    pub pos: Option<PulseCount>,
+    pub pos: Option<PulseCount>, // this is an Option because we don't have a position before the device is homed.
     pub position_limit: Inch,
     pub pulses_per_revolution: u64,
     pub travel_distance_per_turn: Inch,
@@ -39,10 +38,12 @@ pub enum ProcedureExecutionStateEnum {
     Completed,
 }
 
+// This struct contains the state of the procedure.
+// It's separate from the Pi struct for locking purposes -- this can be read when a movement procedure is running.
 #[derive(Debug)]
 pub struct ProcedureExecutionState {
     pub atm: AtomicProcedureExecutionStateEnum,
-    pub seconds_remaining: AtomicU64,
+    pub seconds_remaining: AtomicU64, // number of seconds remaining in the current step
 }
 
 #[derive(juniper::GraphQLObject, Debug, Serialize, Deserialize, Clone)]
@@ -166,21 +167,8 @@ impl From<Procedure> for ProcedureInputObject {
 
 #[derive(juniper::GraphQLObject, Debug, Serialize, Deserialize, Clone)]
 pub struct ProcedureRunStatus {
-    // TODO make sure I come back and decide if these can be nil and make sure the description is updated.
-    // #[graphql(description="CouchDB ID of the currently running procedure. Nil if no procedure is running.")]
-    // current_procedure_id : String,
-    
-    // #[graphql(description="Name of the currently running procedure. Nil if no procedure is running.")]
-    // current_procedure_name : String,
-    
     #[graphql(description="One-indexed number of the current procedure step in the currently running procedure. Nil if no procedure is running.")]
     pub current_procedure_step_number : i32,
-
-    //#[serde(skip)]
-    //current_procedure_step_start_instant: Instant,
-    
-    // #[graphql(description="Start time of the current procedure step in the currently running procedure. Nil if no procedure is running or if the slide holder is currently en route to a staining jar.")]
-    // current_procedure_step_seconds_remaining: String,
     
     #[graphql(description="The cycle number, one-indexed, of how many times the procedure has been repeated in a single run.")]
     pub current_cycle_number: i32,
